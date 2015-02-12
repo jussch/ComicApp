07aacf01e4f3b85f775063dde37b7ba5471940c2
jussch <jaschultz25@gmail.com>
2015-02-11 22:43:07 -0500

    Install Backbone

---
+++ app/assets/javascripts/comic_app.js
@@ -0,0 +1,12 @@
+window.ComicApp = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+  }
+};
+
+$(document).ready(function(){
+  ComicApp.initialize();
+});
